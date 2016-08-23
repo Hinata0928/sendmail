88f134b65b0589c88bf2ff6775f19cff4f12f388
liuhuan <liuhuan@taolx.com>
2016-08-23 11:28:53 +0800

    hide message

--- sendmail.py
+++ sendmail.py
@@ -3,9 +3,9 @@ import smtplib
 from email.header import Header
 from email.mime.text import MIMEText
 server = smtplib.SMTP('smtp.163.com', 25)
-server.login('Hintat0928@163.com', 'lh19900928')
+server.login('xxx@163.com', 'xxx')
 msg = MIMEText('hello, send by Python...', 'plain', 'utf-8')
-msg['From'] = 'Hintat<Hintat0928@163.com>'
+msg['From'] = 'Hintat<xxx@163.com>'
 msg['Subject'] = Header(u'text', 'utf8').encode()
-msg['To'] = 'Hintat<409365364@qq.com>'
-server.sendmail('Hintat0928@163.com', ['409365364@qq.com'], msg.as_string())
+msg['To'] = 'Hintat<xxx@qq.com>'
+server.sendmail('xxx@163.com', ['xxx@qq.com'], msg.as_string())

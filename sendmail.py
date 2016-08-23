b260e728e58f643d1eabb39dcf24c57355082046
Liu Huan <409365364@qq.com>
2016-08-23 15:59:20 +0800

    Update sendmail.py

--- sendmail.py
+++ sendmail.py
@@ -3,9 +3,9 @@ import smtplib
 from email.header import Header
 from email.mime.text import MIMEText
 server = smtplib.SMTP('smtp.163.com', 25)
-server.login('Hintat0928@163.com', 'lh19900928')
+server.login('xxxxxx@xxx.com', 'xxxxxx')
 msg = MIMEText('hello, send by Python...', 'plain', 'utf-8')
-msg['From'] = 'Hintat<Hintat0928@163.com>'
+msg['From'] = 'xxx<xxx@xxx.com>'
 msg['Subject'] = Header(u'text', 'utf8').encode()
-msg['To'] = 'Hintat<409365364@qq.com>'
-server.sendmail('Hintat0928@163.com', ['409365364@qq.com'], msg.as_string())
+msg['To'] = 'xxx<xxxxxx@xxx.com>'
+server.sendmail('xxxxxx@xxx.com', 'xxxxxx@xx.com'], msg.as_string())
